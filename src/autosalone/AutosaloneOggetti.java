2c588556c9a5e12bf880d90312416038bbae2b88
panseri.21255 <panseri.tommaso.studente@itispaleocapa.it>
2024-09-30 13:14:43 +0200

    commit commit2

--- src/autosalone/AutosaloneOggetti.java
+++ src/autosalone/AutosaloneOggetti.java
@@ -8,21 +8,42 @@ public class AutosaloneOggetti {
 
 
 
+
     public String getMarca(){
         return marca;
     }
+    public void setMarca(String marc){
+        marca = marc;
+    }
+
+
 
     public String getModello(){
         return modello;
     }
+    public void setModello(String model){
+        modello = model;
+    }
+
+
 
     public double getPrezzo(){
         return prezzo;
     }
+    public void setPrezzo(double prez){
+        prezzo = prez;
+    }
+
+
 
     public AutosaloneOggetti(String marcaA, String modelloA, double prezzoA) {
         marca = marcaA;
         modello = modelloA;
         prezzo = prezzoA;
+    }
+    public AutosaloneOggetti (){
+        marca = null;
+        modello = null;
+        prezzo = 0.0;
     }
 }
--- src/autosalone/Main.java
+++ src/autosalone/Main.java
@@ -1,17 +1,40 @@
 package autosalone;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String marca = "bmw";
-        String modello = "m4";
-        double prezzo = 0.0;
+        Scanner in = new Scanner (System.in);
 
-        AutosaloneOggetti b1 = new AutosaloneOggetti (marca,modello,prezzo);
 
-        System.out.println ("Marca:" + b1.getMarca());
-        System.out.println ("Modello:" + b1.getModello());
-        System.out.println ("prezzo:" + b1.getPrezzo());
+        AutosaloneOggetti b1 = new AutosaloneOggetti();
+        AutosaloneOggetti b2 = new AutosaloneOggetti();
+        AutosaloneOggetti b3 = new AutosaloneOggetti();
+        AutosaloneOggetti b4 = new AutosaloneOggetti();
 
+        System.out.println ("Marca1:"); b1.setMarca(in.nextLine());
+        System.out.println ("Modello1:"); b1.setModello(in.nextLine());
+        System.out.println ("prezzo1:"); b1.setPrezzo(in.nextDouble());
+        in.nextLine();
+
+        System.out.println ("Marca2:"); b2.setMarca(in.nextLine());
+        System.out.println ("Modello2:"); b2.setModello(in.nextLine());
+        System.out.println ("prezzo2:"); b2.setPrezzo(in.nextDouble());
+        in.nextLine();
+
+        System.out.println ("Marca3:"); b3.setMarca(in.nextLine());
+        System.out.println ("Modello3:"); b3.setModello(in.nextLine());
+        System.out.println ("prezzo3:"); b3.setPrezzo(in.nextDouble());
+        in.nextLine();
+
+        System.out.println ("Marca4:"); b4.setMarca(in.nextLine());
+        System.out.println ("Modello4:"); b4.setModello(in.nextLine());
+        System.out.println ("prezzo4:"); b4.setPrezzo(in.nextDouble());
+        in.nextLine();
+
+        System.out.println("Macchina 1: " + b1.getMarca() + "\t" + b1.getModello() + "\t" + b1.getPrezzo());
+        System.out.println("Macchina 2: " + b2.getMarca() + "\t" + b2.getModello() + "\t" + b2.getPrezzo());
+        System.out.println("Macchina 3: " + b3.getMarca() + "\t" + b3.getModello() + "\t" + b3.getPrezzo());
+        System.out.println("Macchina 4: " + b4.getMarca() + "\t" + b4.getModello() + "\t" + b4.getPrezzo());
     }
 }
